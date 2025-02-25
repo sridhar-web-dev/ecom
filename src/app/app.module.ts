9216149d632d4a1adae1899b629543e2a70f19e3
sridhar-web-dev <webdev.sridhar@gmail.com>
2025-02-25 17:45:46 +0530

    Third Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SideBarComponent } from './home-page/side-bar/side-bar.component';
+import { HomeBannerComponent } from './home-page/home-banner/home-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomePageComponent,
+    SideBarComponent,
+    HomeBannerComponent
   ],
   imports: [
     BrowserModule,
